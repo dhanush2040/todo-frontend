221bd7ac30c285f7babe5e38ad4c8fcbe2fd763d
Madhesh B <madhesh5463@gmail.com>
2026-01-11 23:07:17 +0530

    Server end points updation

--- src/server_url.js
+++ src/server_url.js
@@ -1,2 +1,2 @@
-// export default "https://todo-backend-aneq.onrender.com";
-export default "http://localhost:5000";
+export default "https://todo-backend-aneq.onrender.com";
+// export default "http://localhost:5000";
